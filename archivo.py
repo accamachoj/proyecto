3a7ac542013d0162583b41236a6ba2ee12c2a066
Andrea <acamacho176@unab.edu.co>
2019-03-26 23:41:02 -0500

    Es funcional, no tiene css y la raiz no sirve. Version 1

--- archivo.py
+++ archivo.py
@@ -2,16 +2,51 @@ from flask import Flask
 from flask import render_template
 import MySQLdb
 from flask import request
+from database import DBHelper
 
 app = Flask(__name__)
+diccionario = {}
+db = DBHelper()
+#db.crearTablalogin()
+#db.crearTablaReserva()
+#db.insertarlogin()
+#db.quemada()
 
 @app.route("/")
-def inicio():
-    return render_template('inicio.html')
+def inicio():    
+    #retorno = db.leer("demo1")
+    #print(retorno)
+    #diccionario[retorno[0]] = [retorno[1], retorno[2]]
+    print(diccionario)
+    return render_template('index.html', dict=diccionario)
 
 @app.route("/tabla")
 def index():
-    return render_template('index.html')
+    return render_template('../index.html')
 
 
-app.run(debug=True, port=5000)
+@app.route("/test", methods=['GET', 'POST'])
+def test():
+
+    base = db.todos()
+
+    for item in base:
+        diccionario[item[0]] = [item[1], item[2]]
+
+    if request.method == 'GET':
+        return render_template("test.html", dict = diccionario)
+    elif request.method == 'POST':
+        if "user" in request.form:
+            db.update(request.form['user'], request.form['id'], request.form['estado'])
+            print("hola desde el post: " + request.form["user"])
+        if "uno" in request.form:
+            nombre = request.form['uno']  
+            #d = {'demo1' : ['liss', 'ocupado'], 'demo2' : ['liss', 'ocupado']}
+            return render_template("index.html" , nombre = nombre, dict = diccionario)
+    return '200'
+
+
+
+
+
+app.run(debug=True, port=8000)
